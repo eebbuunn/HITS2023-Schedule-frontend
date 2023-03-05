2d7d07ce2a88dbe5c4d15ef73f7311e6d7ef0fbb
zzizan <romka-pol-16@mail.ru>
2023-03-05 21:56:30 +0700

    Добавли js файл в котором реализовал функции post, get, put и del соотвествующие http методом, а так же функцию refreshWhenExpired, которая обновляет истекший токен. Реализовал аторизацию пользователя. Немного изменил набар добавиви nav-logo и navbar-brand margin.

---
+++ scripts/requests.js
@@ -0,0 +1,64 @@
+export function post(url, body) {
+    return fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: new Headers({
+            "Authorization": "Bearer " + localStorage.getItem("userToken"),
+            'Content-Type': 'application/json'
+        })
+    })
+        .then(r => {
+            if(r.status == 401) RefreshWhenExpired();
+            else return r;
+        })
+
+}
+
+export function get(url) {
+    return fetch(url, {
+        headers: new Headers({
+            "Authorization": "Bearer " + localStorage.getItem("userToken"),
+        })
+    })
+        .then(response => {
+            return response.json()
+        });
+}
+
+export function put(url, body) {
+    return fetch(url, {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: new Headers({
+            "Authorization": "Bearer " + localStorage.getItem("userToken"),
+            'Content-Type': 'application/json'
+        })
+    })
+        .then(r => {
+            if(r.status == 401) RefreshWhenExpired();
+            else return r;
+        });
+}
+
+export function del(url) {
+    return fetch(url, {
+        method: 'DELETE',
+        headers: new Headers({
+            "Authorization": "Bearer " + localStorage.getItem("userToken"),
+            'Content-Type': 'application/json'
+        })
+    })
+        .then(r => {
+            if(r.status == 401) RefreshWhenExpired();
+            else return r;
+        });
+}
+
+function RefreshWhenExpired() {
+    post('http://v1683738.hosted-by-vdsina.ru:5000/auth/refresh', localStorage.getItem('refreshUserToken'))
+        .then(r => {
+            let json = r.json();
+            localStorage.setItem("userToken", json.accessToken);
+            localStorage.setItem("refreshUserToken", json.refreshToken)
+        });
+}
