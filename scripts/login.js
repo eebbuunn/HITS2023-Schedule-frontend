d272361081df9688bbccd47c0b5a62d191cc7b8e
mironovaEv <mironovaev26@gmail.com>
2023-03-06 23:15:36 +0700

    Выполнена верстка страницы со всеми пользователями

--- scripts/login.js
+++ scripts/login.js
@@ -1,43 +1,38 @@
-import { post } from "./requests.js";
+import { post } from './requests.js';
 
-$(document).ready(function (){
-    setLoginButtonEvent();
-    registerLoginFieldsEvent();
+$(document).ready(function () {
+  setLoginButtonEvent();
+  registerLoginFieldsEvent();
 });
 
 export function setLoginButtonEvent() {
-    $("#login-form").submit((e) => {
-        e.preventDefault();
-        let requestsBody =  {
-            login: $("#login").val().trim(),
-            password: $("#password").val().trim()
-        }
+  $('#login-form').submit((e) => {
+    e.preventDefault();
+    let requestsBody = {
+      login: $('#login').val().trim(),
+      password: $('#password').val().trim(),
+    };
 
-
-        post('http://v1683738.hosted-by-vdsina.ru:5000/auth/login/web', requestsBody)
-            .then(async (response) =>{
-                if (response.ok){
-                    let json = await response.json();
-                    console.log(json);
-                    localStorage.setItem('userToken', json.accessToken)
-                    localStorage.setItem('refreshUserToken', json.refreshToken)
-                    window.location.href = '../pages/mainpage.html'
-
-                }
-                else{
-                    let json = await response.json();
-                    console.log(json);
-                    $("#login-form").addClass("invalid");
-                    $("#login-form").removeClass("valid");
-                }
-            })
-
+    post('http://v1683738.hosted-by-vdsina.ru:5000/auth/login/web', requestsBody).then(async (response) => {
+      if (response.ok) {
+        let json = await response.json();
+        console.log(json);
+        localStorage.setItem('userToken', json.accessToken);
+        localStorage.setItem('refreshUserToken', json.refreshToken);
+        window.location.href = '../pages/mainpage.html';
+      } else {
+        let json = await response.json();
+        console.log(json);
+        $('#login-form').addClass('invalid');
+        $('#login-form').removeClass('valid');
+      }
     });
+  });
 }
 
 export function registerLoginFieldsEvent() {
-    $(".form-control").on("input", function() {
-        $(this).parent().parent().addClass("valid");
-        $(this).parent().parent().removeClass("invalid");
-    });
+  $('.form-control').on('input', function () {
+    $(this).parent().parent().addClass('valid');
+    $(this).parent().parent().removeClass('invalid');
+  });
 }
