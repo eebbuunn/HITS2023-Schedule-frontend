d936d039828f2646b216ed6d5af22a9d00bc621f
zzizan <romka-pol-16@mail.ru>
2023-03-08 18:48:49 +0700

    реализовал обработку роли админа

--- scripts/mainpage.js
+++ scripts/mainpage.js
@@ -7,7 +7,11 @@ $(document).ready(function () {
 function navbarChek(){
     get(`http://v1683738.hosted-by-vdsina.ru:5000/users/me`)
         .then(profile => {
-            $("#navbar").find("#nickname").text(profile.login);
+            $("#nickname").text(profile.login);
+            if (!isUserAdmin(profile.roles)){
+                $("#users").addClass("d-none")
+                $("#button-users").addClass("d-none")
+            }
             $("#signout").click(() => {
                 post(`http://v1683738.hosted-by-vdsina.ru:5000/auth/logout`)
                     .then(() => {
@@ -19,4 +23,13 @@ function navbarChek(){
             // todo добавить обработку ролей
             localStorage.setItem("userId", profile.id);
         })
+}
+
+function isUserAdmin(roles){
+    if (roles.includes(3) || roles.includes(4)){
+        return true
+    }
+    else{
+        return false
+    }
 }
