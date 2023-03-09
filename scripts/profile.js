5273a909c4fc03f92b8124f3376fd31606aa3125
mironovaEv <mironovaev26@gmail.com>
2023-03-09 15:51:27 +0700

    Добавлена возможность удаления пользователя

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,1 +1,13 @@
-$(document).ready(function () {});
+import { del } from './requests.js';
+async function deleteUser(user_id) {
+  const url = 'http://v1683738.hosted-by-vdsina.ru:5000/users/' + user_id;
+  const response = await del(url);
+  window.location.href = 'users.html';
+}
+
+$(document).ready(function () {
+  const user_id = window.location.href.substring(41);
+  $(document).on('click', '#delete_user', function () {
+    deleteUser(user_id);
+  });
+});
