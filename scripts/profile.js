f78cdee00d93dd90e455954e74d57b60edcffdef
mironovaEv <mironovaev26@gmail.com>
2023-03-12 17:58:39 +0700

    Добавлено всплывающее окно для отображения ошибок

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,9 +1,18 @@
-import { del, get } from './requests.js';
+import { del, get, put } from './requests.js';
 
 async function deleteUser(user_id) {
   const url = 'http://v1683738.hosted-by-vdsina.ru:5000/users/' + user_id;
   const response = await del(url);
-  window.location.href = 'users.html';
+  if (response.ok) {
+    $('#error').addClass('d-none');
+    window.location.href = 'users.html';
+  } else {
+    response.text().then((text) => {
+      $('#deleteModal').modal('hide');
+      $('#errorModal').find('.modal-body').text(JSON.parse(text).errors.message);
+      $('#errorModal').modal('show');
+    });
+  }
 }
 
 function fillGroups(group_number) {
@@ -59,18 +68,55 @@ async function loadProfile(user_id) {
       case 'ADMIN':
         block.find('#adminCheck').attr('checked', true);
         break;
-      case 'ROOT':
-        block.find('#rootCheckй').attr('checked', true);
-        break;
     }
   }
   block.removeClass('d-none');
+}
+async function editUser(user_id) {
+  const url = 'http://v1683738.hosted-by-vdsina.ru:5000/users/' + user_id;
+  let roles = [];
+  $('input:checkbox[name=checkbox]:checked').each(function () {
+    roles.push($(this).val());
+  });
+  const group = $('#input-group').val();
+  const teacher_id = $('#input-teacher').val();
+  const body = {
+    roles: roles,
+    teacherId: teacher_id,
+    group: Number(group),
+  };
+  const response = await put(url, body);
+  if (response.ok) {
+    loadProfile(user_id);
+  } else {
+    response.text().then((text) => {
+      $('#errorModal').find('.modal-body').text(JSON.parse(text).errors.message);
+      $('#errorModal').modal('show');
+    });
+  }
 }
 
 $(document).ready(function () {
   const user_id = window.location.href.substring(41);
   loadProfile(user_id);
+  $('#studentCheck').change(function () {
+    if ($('#studentCheck').prop('checked')) {
+      $('.input-group-container').removeClass('d-none');
+    } else {
+      $('.input-group-container').addClass('d-none');
+    }
+  });
+  $('#teacherCheck').change(function () {
+    if ($('#teacherCheck').prop('checked')) {
+      $('.input-teacher-container').removeClass('d-none');
+    } else {
+      $('.input-teacher-container').addClass('d-none');
+    }
+  });
   $(document).on('click', '#delete_user', function () {
     deleteUser(user_id);
+  });
+  $(document).on('click', '#edit_user', function () {
+    editUser(user_id);
   });
 });
